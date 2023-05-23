3d6d4fc667e214627d897f2531c5a7d8deb01a79
osancho <oscarsanchopajares@hotmail.com>
2023-05-23 08:46:27 +0200

    remove console.log

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -4,8 +4,6 @@ import { ThemeContext } from '../context/ThemeContext'
 export const DarkModeSwitch = () => {
   const { isDarkMode, toggleTheme } = useContext(ThemeContext)
 
-  console.log('isDarkMode', isDarkMode)
-
   return (
     <button onClick={toggleTheme}>
       Switch to {isDarkMode ? 'light' : 'dark'} mode
