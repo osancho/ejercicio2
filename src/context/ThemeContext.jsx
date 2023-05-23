2feb22b2b7efa89d6b503c6df1aff6dde5f74fa4
osancho <oscarsanchopajares@hotmail.com>
2023-05-23 08:48:48 +0200

    guardar tema en localStorage

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,13 +1,21 @@
-import { createContext, useState } from 'react'
+import React, { useEffect } from 'react'
 
-export const ThemeContext = createContext()
+export const ThemeContext = React.createContext()
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [isDarkMode, setIsDarkMode] = React.useState(() => {
+    const storedTheme = window.localStorage.getItem('isDarkMode')
+    return storedTheme ? JSON.parse(storedTheme) : false
+  })
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode)
   }
+
+  useEffect(() => {
+    window.localStorage.setItem('isDarkMode', JSON.stringify(isDarkMode))
+  }, [isDarkMode])
+
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
       {children}
